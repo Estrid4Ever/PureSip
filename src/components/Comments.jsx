db9ed5b362616015b48732f1ef9f3015bb01d0b8
Estrid4Ever <145568741+Estrid4Ever@users.noreply.github.com>
2024-10-14 18:33:15 +0200

    work on comments
    work on comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
+
 export default function Comments({ recipeId }) {
 
+    const [newComment, setNewComment] = useState("");
+    const [commentName, setCommentName] = useState("");
 
+    const publishNewComment = () => {
+
+        console.log("send");
+    };
+
+    const cancelComment = () => {
+        setNewComment("")
+        setCommentName("")
+    };
 
     return <>
         <div className="comments-container">
@@ -12,11 +25,17 @@ export default function Comments({ recipeId }) {
                     <p className="comments-total-amount">(4st)</p>
                 </div>
 
-                <input type="text" className="comments-text-input" placeholder="Skriv din kommentar" required/>
+                <textarea className="comments-text-input" placeholder="Skriv din kommentar" value={newComment} onChange={e => setNewComment(e.target.value)} />
 
-                <div className="comments-buttons">
-                    <button className="comments-button cancel" >Avbryt</button>
-                    <button className="comments-button send" >Skicka</button>
+                <div className="comments-name-buttons">
+                    
+                    <input className="comments-name" type="text" placeholder="Ange namn" value={commentName} onChange={e => setCommentName(e.target.value)}/>
+
+                    <div className="comments-buttons">
+                        <button onClick={() => cancelComment()} className="comments-button cancel" >Avbryt</button>
+                        <button onClick={publishNewComment} className="comments-button send" >Skicka</button>
+                    </div>
+
                 </div>
 
             </div>
@@ -24,10 +43,10 @@ export default function Comments({ recipeId }) {
             <ul className="comments-list">
                 <li>
                     <div className="comments-name-date">
-                        <h3>name</h3>
+                        <h4>name</h4>
                         <p>date</p>
                     </div>
-                    <p>kommentaren lyder så här! bla bla bla...</p>
+                    <p className="comments-text">kommentaren lyder så här! bla bla bla...</p>
                 </li>
             </ul>
 
