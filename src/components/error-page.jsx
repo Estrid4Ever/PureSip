76212eeabf2117f0b5be007040db8f8c68ede4e5
Estrid4Ever <145568741+Estrid4Ever@users.noreply.github.com>
2024-10-14 15:24:06 +0200

    troubleshooting
    troubleshooting

--- src/components/error-page.jsx
+++ src/components/error-page.jsx
@@ -3,6 +3,7 @@ import { useRouteError } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
+  //lala
 
   return (
     <div id="error-page">
