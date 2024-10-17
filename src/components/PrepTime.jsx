da9f66c9c75ef14c939e874d9d30fce80bacb41b
Yosef Matiwos <Yosef.Matiwos@iths.se>
2024-10-17 18:39:26 +0200

    Added degree of difficultys in the homePage

--- src/components/PrepTime.jsx
+++ src/components/PrepTime.jsx
@@ -1,6 +1,19 @@
 export default function PrepTime({ dish }) {
-    return (<div className="prep-time">
-        <i className="fa clock">&#xf017;</i>
-        <p className="time">{dish.timeInMins} min</p>
-    </div>);
+    let difficulty = "";
+
+    // Kontrollera tiden för att bestämma svårighetsgraden
+    if (dish.timeInMins <= 3) {
+        difficulty = "Lätt";
+    } else if (dish.timeInMins >= 4 && dish.timeInMins <= 5) {
+        difficulty = "Medel";
+    } else {
+        difficulty = "Svår";
+    }
+
+    return (
+        <div className="prep-time">
+            <i className="fa clock">&#xf017;</i>
+            <p className="time">{dish.timeInMins} min - {difficulty}</p>
+        </div>
+    );
 }
