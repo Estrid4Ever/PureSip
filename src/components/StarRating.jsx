39136c183566344ef502a3f676595c87d1f84be1
johsteran <johannes-sr@hotmail.com>
2024-10-29 11:53:37 +0100

    work on star rating
    work on partially checked stars

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -8,8 +8,22 @@ export default function StarRating({ dish }) {
 
     let starClass = ["fa fa-star", "fa fa-star", "fa fa-star", "fa fa-star", "fa fa-star"];
 
+    let endIndex;
+
     for (let i = 0; i < dish.avgRating - 1; i++) {
         starClass[i] = starClass[i] + " checked";
+        endIndex = i;
+    }
+
+    if(endIndex < 5) {
+        var decimal = dish.avgRating % 1;
+        var rounded = Math.round(decimal * 10) / 10
+
+        console.log(dish.title + " rating-decimal: " + decimal)
+
+        //switch för 0.25, 0.5 och 0.75
+
+        starClass[endIndex + 1] = starClass[endIndex + 1] + " partial-checked";
     }
 
     const stars = starClass.map((star, index) =>
